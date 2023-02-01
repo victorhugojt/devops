c543ae15650c9c138add9fe7ea0ed4588dc95d4b
Victor Hugo Jimenez T <victor.jimenezt@perficient.com>
2023-02-01 07:36:57 -0500

    vagrant init

--- devops_iac/local_iac/provisioning_impl.py
+++ devops_iac/local_iac/provisioning_impl.py
@@ -1,3 +1,19 @@
+import vagrant
+
+
 def process():
     print('Deploying VM using vagrant')
-    pass
+
+    vagrantfile = '/vm_config.vagrantfile'
+
+    # Initialize the Vagrant environment
+    v = vagrant.Vagrant(vagrantfile)
+
+    # Start the virtual machine
+    v.up()
+
+    # Verify that the virtual machine is running
+    print(v.status())
+
+    # Stop the virtual machine
+    # v.halt()
