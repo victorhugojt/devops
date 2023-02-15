fa918e14d9a81e9216eac62d89cba971bec6d740
Victor Hugo Jimenez T <victor.jimenezt@perficient.com>
2023-02-15 08:43:20 -0500

    install docker

--- devops_iac/startup/common.sh
+++ devops_iac/startup/common.sh
@@ -1,1 +1,6 @@
-#Install Docker, find a git base image
+sudo apt-get update
+
+sudo apt install git
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
