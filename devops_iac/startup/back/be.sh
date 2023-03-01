a8fb1e214239dfb602361f422f68c72600721263
Victor Hugo Jimenez T <victor.jimenezt@perficient.com>
2023-03-01 09:46:38 -0500

    docker up

--- devops_iac/startup/back/be.sh
+++ devops_iac/startup/back/be.sh
@@ -1,5 +1,15 @@
-
 docker pull victorhugojt/devops_rampup_front
 
 # Docker mapped the 8080 port inside of the container to the port 49160 on your machine
-docker run --env-file ./env.list -p 49160:8082 -d victorhugojt/devops_rampup_back
+docker run --env-file ./env.list -p 8083:8083 -d victorhugojt/devops_rampup_users
+
+
+docker build -t victorhugojt/devops_rampup_users .
+
+docker build -t victorhugojt/devops_rampup_log_processor .
+
+docker build -t victorhugojt/devops_rampup_todos .
+
+docker build -t victorhugojt/devops_rampup_front .
+
+docker build -t victorhugojt/devops_rampup_auth .
