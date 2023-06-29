a76ec526fb3ef47bc1868187b899bee9c4c95159
Victor Hugo Jimenez T <victor.jimenezt@perficient.com>
2023-06-29 08:22:31 -0500

    refact

--- devops_iac/cloud_aws/key_pair.tf
+++ devops_iac/cloud_aws/key_pair.tf
@@ -1,8 +1,4 @@
 resource "aws_key_pair" "vhjt_key" {
   key_name   = "vhjt_key"
   public_key = file("/Users/victor.jimenezt/ssh-keys/vjimenezt_public_key.public")
-  tags = {
-    project     = var.project,
-    responsible = var.responsible
-  }
 }
