48e8db207007387e433045ad58c006ac433f508b
Victor Hugo Jimenez T <victor.jimenezt@perficient.com>
2023-09-22 09:13:21 -0500

    tgs

--- devops_iac/cloud_aws/application_load_balancers.tf
+++ devops_iac/cloud_aws/application_load_balancers.tf
@@ -1,3 +1,25 @@
+resource "aws_lb_target_group" "front_target_group" {
+  target_type = var.tg_target_type
+  protocol    = var.tg_protocol
+  port        = var.front_port
+  vpc_id      = data.aws_vpc.vpc.id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 5
+    unhealthy_threshold = 2
+  }
+
+  tags = {
+    project     = var.project,
+    responsible = var.responsible
+  }
+}
+
 resource "aws_lb" "front_application_lb" {
   name               = var.front_lb_name
   load_balancer_type = var.lb_type
@@ -13,7 +35,7 @@ resource "aws_lb_listener" "http_front" {
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_autoscaling_group.frontend_host_auto_scaling_group.arn
+    target_group_arn = aws_lb_target_group.front_target_group.arn
   }
 
 }
