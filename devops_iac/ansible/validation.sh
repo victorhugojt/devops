126c47533c25b485ce2f2cec997922a47b23f8cd
Victor Hugo Jimenez T <victor.jimenezt@perficient.com>
2023-11-27 11:09:21 -0500

    log_processor ansible ok

--- devops_iac/ansible/validation.sh
+++ devops_iac/ansible/validation.sh
@@ -7,3 +7,5 @@ ansible-playbook -i hosts ~/training/devops/devops_iac/ansible/playbooks/first-p
 ansible-playbook -i hosts ~/training/devops/devops_iac/ansible/playbooks/auth.yaml -u ubuntu
 
 ansible-playbook -i hosts ~/training/devops/devops_iac/ansible/playbooks/redis.yaml -u ubuntu
+
+ansible-playbook -i hosts ~/training/devops/devops_iac/ansible/playbooks/log_processor.yaml -u ubuntu
