c45e70a36721b8b654c26083ec212fb1b5f9f41e
Victor Hugo Jimenez T <victor.jimenezt@perficient.com>
2023-11-27 11:26:33 -0500

    todos ansiebl ok

--- devops_iac/ansible/validation.sh
+++ devops_iac/ansible/validation.sh
@@ -9,3 +9,5 @@ ansible-playbook -i hosts ~/training/devops/devops_iac/ansible/playbooks/auth.ya
 ansible-playbook -i hosts ~/training/devops/devops_iac/ansible/playbooks/redis.yaml -u ubuntu
 
 ansible-playbook -i hosts ~/training/devops/devops_iac/ansible/playbooks/log_processor.yaml -u ubuntu
+
+ansible-playbook -i hosts ~/training/devops/devops_iac/ansible/playbooks/todos.yaml -u ubuntu
